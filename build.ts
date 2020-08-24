1a9b043bfbaf33c634845797682dbdf2a5f72ea5
Casper Beyer <caspervonb@pm.me>
2020-08-24 14:12:09 +0800

    Flatten build script
    This flattens the build script to be a standalone script without any
    exports.

--- build.ts
+++ build.ts
@@ -1,27 +1,25 @@
-export default async function build() {
-  try {
-    await Deno.mkdir("lib");
-  } catch (err) {
-    // no-op
-  }
-
-  const [diagnostics, records] = await Deno.compile(
-    "src/wasi_snapshot_preview1.ts",
-    undefined,
-    {
-      outDir: "lib",
-    },
-  );
-
-  if (diagnostics) {
-    throw Error(diagnostics.join(" "));
-  }
-
-  for (const [filepath, source] of Object.entries(records)) {
-    await Deno.writeTextFile(filepath, source);
-  }
+try {
+  await Deno.mkdir("lib");
+} catch (err) {
+  // no-op
 }
 
-if (import.meta.main) {
-  build();
+const [diagnostics, records] = await Deno.compile(
+  "src/wasi_snapshot_preview1.ts",
+  undefined,
+  {
+    outDir: "lib",
+  },
+);
+
+if (diagnostics) {
+  for (const diagnostic of diagnostics) {
+    console.error(diagnostics);
+  }
+
+  Deno.exit(1);
+}
+
+for (const [filepath, source] of Object.entries(records)) {
+  await Deno.writeTextFile(filepath, source);
 }
